e0533b83e6dd71088b219fdaa274cd8359dd9c8d
Maria Gabriela <mgvillarreal09@gmail.com>
2025-03-17 17:11:56 -0300

    add handlebars

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
+import ProductManager from './managers/ProductManager.js';
 import { engine } from 'express-handlebars';
 
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true }));
+
+const productManager = new ProductManager('./src/data/products.json');
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -12,6 +17,16 @@ app.set('views', './src/views');
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
+
+app.get('/', async (req, res) =>{
+    try{
+        const products = await productManager.getAll();
+        res.render('home', {products});
+    } catch(error){
+        console.error('Error al obtener los productos:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
 
 app.listen(8080, () => {
     console.log('Servidor escuchando en http://localhost:8080');
