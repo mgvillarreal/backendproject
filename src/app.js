319b467cf03145f094118f4445e9c3daae94b1e9
Maria Gabriela <mgvillarreal09@gmail.com>
2025-03-17 17:44:45 -0300

    add socket.io

--- src/app.js
+++ src/app.js
@@ -3,21 +3,31 @@ import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 import ProductManager from './managers/ProductManager.js';
 import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
+import viewsRouter from './routes/views.router.js';
+import __dirname from './utils.js';
 
 const app = express();
-app.use(express.json());
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true }));
 
 const productManager = new ProductManager('./src/data/products.json');
 
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+app.use(express.urlencoded({extended:true}));
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './src/views');
+app.set('views', `${__dirname}/views`);
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
+app.use('/', viewsRouter);
 
+const httpServer = app.listen(8080, () => console.log('Servidor escuchando en http://localhost:8080'));
+const io = new Server(httpServer);
+app.set('socketio', io);
+
+/* ------------------- view ------------------- */
 app.get('/', async (req, res) =>{
     try{
         const products = await productManager.getAll();
@@ -28,6 +38,24 @@ app.get('/', async (req, res) =>{
     }
 });
 
-app.listen(8080, () => {
-    console.log('Servidor escuchando en http://localhost:8080');
+/* ------------------- socket.io ------------------- */
+io.on('connection', async (socket) => {
+    console.log('Nuevo cliente conectado');
+
+    const products = await productManager.getAll();
+    socket.emit('actualizarProductos', products);
+
+    // Add product
+    socket.on('nuevoProducto', async (producto) => {
+        await productManager.addProduct(producto);
+        const productosActualizados = await productManager.getAll();
+        io.emit('actualizarProductos', productosActualizados); // Emitimos a todos los clientes
+    });
+
+    // Delete product
+    socket.on('eliminarProducto', async (id) => {
+        await productManager.deleteProduct(parseInt(id));
+        const productosActualizados = await productManager.getAll();
+        io.emit('actualizarProductos', productosActualizados); // Emitimos a todos los clientes
+    });
 });
